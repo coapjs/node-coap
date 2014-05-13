029f94b49cd9b58777c082660fd19e663d64f0a9
Nguyen Quoc Dinh <nqdinh@ubisen.com>
2014-05-13 22:20:49 +0700

    block2 for non obs works, not for obs block2 type yet

--- examples/client.js
+++ examples/client.js
@@ -1,5 +1,5 @@
 const coap  = require('../') // or coap
-    , req   = coap.request('coap://localhost/Matteo')
+    , req   = coap.request('coap://[aaaa::11:22ff:fe33:4403]/.well-known/core')
 
 req.on('response', function(res) {
   res.pipe(process.stdout)
