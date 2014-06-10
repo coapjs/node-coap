a099cf5cd727b98967770a1bdcd0c9764017b4f5
Nguyen Quoc Dinh <nqdinh@ubisen.com>
2014-06-10 22:58:06 +0700

    more error checking

--- nqd/test-block2.js
+++ nqd/test-block2.js
@@ -11,25 +11,30 @@
 
 const coap        = require('./..')
     , server      = coap.createServer()
-    , payload     = new Buffer(100000)
-    // , payload     = new Buffer("Centre-right leaders from Sweden, Germany, Britain and the Netherlands are meeting near Stockholm to try to reach a consensus on European reform. The controversial question of who is to head the European Commission is likely to be discussed, but not officially. UK PM David Cameron is expected to try to get leaders on-side to block Jean-Claude Juncker taking the job. Continue reading the main story “ Start Quote We dislike the idea of presenting front-runners from the different parties because we think that twists the balance between the institutions and the Lisbon treaty” Fredrik Reinfeldt Swedish Prime Minister It sets him against German Chancellor Angela Merkel, who publicly supports the ex-Luxembourg leaders appointment. EU leaders have traditionally named the Commission head on their own, but new rules mean they now have to take into account the results of the European Parliament elections. The European People's Party (EPP) - the largest centre-right grouping in the parliament, of which Mr Juncker is a member - won the highest number of seats in May's polls, and he has argued that gives him the mandate. But observers say neither he nor any other candidate has managed to obtain a majority so far. Some EU leaders, including Mr Cameron, insist that the Commission's priorities need to be re-defined first before an appropriate candidate is chosen. Commission president is the most powerful job in Brussels, shaping EU policy in key areas such as economic reform, immigration and ties with other global powers. 'Stitch-up'Few details from the mini-summit in Harpsund have emerged. However, job creation, institutional changes in the EU and structural reforms to boost EU competitiveness were said to be high on the agenda. The UK, Sweden and the Netherlands are leading a campaign to block Mr Juncker's candidacy, which has the support of the EPP.")
+    , payload     = new Buffer(10000)
 
-const maxBlock2   = 16
+const maxBlock2   = 1024    //16, 32, 64, must <= 2**(6+4)
 var totalBlock    = Math.ceil(payload.length/maxBlock2)
 var numberBlock   = 0 
 var isLastBlock   = false
 
-
+// server
+// play split up for blockwise 
 server.on('request', function(req, res) {
   if (++numberBlock == totalBlock) 
     isLastBlock = true
 
   var requestedBlockOption = _parseBlock2(req.options) 
-  if (!requestedBlockOption) requestedBlockOption = {
+  // for the first request, block2 option may be missed
+  if (!requestedBlockOption) 
+    requestedBlockOption = {
       size: maxBlock2,
-      moreBlock2: 1,
       num: 0
     }
+  // block2 size should not bigger maxBlock2
+  if (requestedBlockOption.size > maxBlock2) 
+    requestedBlockOption.size = maxBlock2
+
   console.log('--------------------------')
   console.log('req block2 ', requestedBlockOption)
   // console.log(requestedBlockOption)
@@ -43,6 +48,9 @@ server.on('request', function(req, res) {
 // the default CoAP port is 5683
 server.listen(function() {
   var req = coap.request('coap://localhost/Matteo')
+  // test early negotiation
+  req.setOption('Block2', new Buffer([0x0])) // block size of 1024
+  // test later negotiation (?)
   req.on('response', function(res) {
     // console.log(res)
     // console.log('receive')
@@ -73,12 +81,17 @@ function _createBlock(requestedBlock, isLastBlock) {
 
   // total num occupy up to 5 octets
   // num share the higher octet of first byte, and (may) take more 2 bytes for the rest 4 octets
-  if (num <=0xfff && num > 0xf) {
+  if (num <= 0xf) { 
+    extraNum = null
+  }
+  else if (num <=0xfff) {
     extraNum = new Buffer([num/16])
-  } else if (num <=0xfffff) {
+  } 
+  else if (num <=0xfffff) {
     extraNum = new Buffer(2)
     extraNum.writeUInt16BE(num>>4,0)
-  } else {
+  } 
+  else {
     throw new Error('too big request')
   }
 
@@ -103,6 +116,10 @@ function _parseBlock2(options) {
         break
         default:
         throw new Error('Too long block2 option size: '+block2Value.length)
+      }
+      // limit value of size is 1024 (2**(6+4))
+      if (block2Value.slice(-1)[0] == 7) {
+        throw new Error('Block size should not bigger than 1024')
       }
       return {
         moreBlock2: (block2Value.slice(-1)[0] & (0x01<<3))? true:false,
