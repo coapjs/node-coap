904dc2e7e9fc68c064e6e3af4769994500899556
Nguyen Quoc Dinh <nqdinh@ubisen.com>
2014-06-16 22:58:20 +0700

    402 error handler and test

--- test/blockwise.js
+++ test/blockwise.js
@@ -7,8 +7,7 @@ describe('blockwise2', function() {
     , payload   = new Buffer(1300)
 
   beforeEach(function(done) {
-    // port = nextPort()
-    port = 10000
+    port = nextPort()
     server = coap.createServer()
     server.listen(port, done)
   })
@@ -36,7 +35,6 @@ describe('blockwise2', function() {
   })
 
   it('should use blockwise in response when payload bigger than max packet', function(done) {
-    // var payload   = new Buffer(100)         // default max packet is 1280
     var req = coap.request({
         port: port
     })
@@ -75,7 +73,7 @@ describe('blockwise2', function() {
     var req = coap.request({
         port: port
     })
-    .setOption('Block2', new Buffer([0x02])) // reques from block 1, with size = 32
+    .setOption('Block2', new Buffer([0x02]))
     .on('response', function(res) {
       var block2
       for (var i in res.options) {
@@ -94,28 +92,41 @@ describe('blockwise2', function() {
     })
   })
 
-  // it('should receive error when early negotation request block size higher than 1024', function(done) {
-  //   var req = coap.request({
-  //       port: port
-  //   })
-  //   .setOption('Block2', new Buffer([0x07])) // reques from block 1, with size = 32
-  //   .on('response', function(res) {
-  //     // expect(block2 instanceof Buffer).to.eql(true)
-  //     // expect(block2[block2.length-1] & 0x07).to.eql(2)
-  //     console.log(res.code)
-  //     setImmediate(done)
-  //   })
-  //   .end()
-  //   server.on('request', function(req, res) {
-  //     res.end(payload)
-  //   })
-  // })
-
-  it('should receive a right block', function(done) {
+  it('should receive error when early negotation request block size higher than 1024', function(done) {
     var req = coap.request({
         port: port
     })
-    .setOption('Block2', new Buffer([0x10])) // reques from block 1, with size = 16
+    .setOption('Block2', new Buffer([0x07])) // request for block 0, with overload size of 2**(7+4)
+    .on('response', function(res) {
+      expect(res.code).to.eql('4.02')
+      setImmediate(done)
+    })
+    .end()
+    server.on('request', function(req, res) {
+      res.end(payload)
+    })
+  })
+
+  it('should receive error request for out of range block number', function(done) {
+    var req = coap.request({
+        port: port
+    })
+    .setOption('Block2', new Buffer([0x55])) // request for block 5, size = 512 from 1300B msg (total 1300/512=3 blocks)
+    .on('response', function(res) {
+      expect(res.code).to.eql('4.02')
+      setImmediate(done)
+    })
+    .end()
+    server.on('request', function(req, res) {
+      res.end(payload)
+    })
+  })
+
+  it('should be able to receive part of message', function(done) {
+    var req = coap.request({
+        port: port
+    })
+    .setOption('Block2', new Buffer([0x10])) // request from block 1, with size = 16
     .on('response', function(res) {
       expect(res.payload).to.eql(payload.slice(1*16, payload.length+1))
       setImmediate(done)
@@ -126,12 +137,12 @@ describe('blockwise2', function() {
     })
   })
 
-  it('should receive a full response payload', function(done) {
+  it('should receive full response payload', function(done) {
     var payload = new Buffer(16*0xff+1)
     var req = coap.request({
         port: port
     })
-    .setOption('Block2', new Buffer([0x0])) // requested block size = 16, almost 10000/16 = 63 block
+    .setOption('Block2', new Buffer([0x0])) // early negotation with block size = 16, almost 10000/16 = 63 blocks
     .on('response', function(res) {
       expect(res.payload).to.eql(payload)
       setImmediate(done)
