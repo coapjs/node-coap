cb4cac99a586bcfee57967169894072509d35004
Matteo Collina <matteo.collina@gmail.com>
2014-07-02 12:13:45 +0200

    Added copyright notice in all tests.

--- test/request.js
+++ test/request.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2013-2014 node-coap contributors.
+ *
+ * node-coap is licensed under an MIT +no-false-attribs license.
+ * All rights not explicitly granted in the MIT license are reserved.
+ * See the included LICENSE file for more details.
+ */
+
 var coap      = require('../')
   , parse     = require('coap-packet').parse
   , generate  = require('coap-packet').generate
