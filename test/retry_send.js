23870779268544cc396006d0ed67e212e4c9e60f
michuNEEO <michael@neeo.com>
2017-05-19 22:20:38 +0200

    fix invalid factory method, also add tests

--- test/retry_send.js
+++ test/retry_send.js
@@ -14,4 +14,14 @@ describe('RetrySend', function() {
     expect(result._maxRetransmit).to.eql(55)
   })
 
+  it('should use default retry count, using the retry_send factory method', function() {
+    var result = RetrySend({}, 1234, 'localhost')
+    expect(result._maxRetransmit).to.eql(parameters.maxRetransmit)
+  })
+
+  it('should use a custom retry count, using the retry_send factory method', function() {
+    var result = RetrySend({}, 1234, 'localhost', 55)
+    expect(result._maxRetransmit).to.eql(55)
+  })
+
 })
