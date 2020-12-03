26a96e19df7fc91f24b168fa2c84efdb4c58b3ff
Braydon Harris <braydonharris@gmail.com>
2020-12-03 13:10:04 -0700

    Add some unit tests

--- test/blockwise.js
+++ test/blockwise.js
@@ -11,6 +11,7 @@ var coap        = require('../')
   , generate    = require('coap-packet').generate
   , getOption   = require('../lib/helpers').getOption
   , parseBlock2 = require('../lib/helpers').parseBlock2
+  , parseBlock1 = require('../lib/helpers').parseBlock1
   , dgram       = require('dgram')
 
 describe('blockwise2', function() {
@@ -352,4 +353,176 @@ describe('blockwise2', function() {
 
     parallelBlock2Test(done, checkNothing, checkBlock2Payload, checkNormalRespPayload)
   })
+})
+
+describe.only('blockwise1', () =>{
+  var server
+  , port
+  , clientPort
+  , client
+  , bufferVal
+  , payload   = Buffer.alloc(1536)
+
+  beforeEach(function(done) {
+    bufferVal = 0
+    port = nextPort()
+    server = coap.createServer()
+    server.listen(port, done)
+  })
+
+  beforeEach(function(done) {
+    clientPort = nextPort()
+    client = dgram.createSocket('udp4')
+    client.bind(clientPort, done)
+  })
+
+  afterEach(function() {
+    server.close()
+    client.close()
+  })
+
+  function send(message) {
+    client.send(message, 0, message.length, port, '127.0.0.1')
+  }
+
+  function nextBufferVal() {
+    if (bufferVal > 255)
+      bufferVal = 0
+    return bufferVal++
+  }
+
+  function fillPayloadBuffer(buffer) {
+    for (var i = 0; i < buffer.length; i++) {
+      buffer[i] = nextBufferVal()
+    }
+    return buffer
+  }
+
+  it('should server not use blockwise in response when payload fit in one packet', function(done) {
+    var payload   = Buffer.alloc(100)         // default max packet is 1280
+
+    var req = coap.request({
+        port: port
+    })
+    .on('response', function(res) {
+      var blockwiseResponse = false
+      for (var i in res.options) {
+        if (res.options[i].name == 'Block1') {
+          blockwiseResponse = true
+          break
+        }
+      }
+      expect(blockwiseResponse).to.eql(false)
+      //expect(cache.get(res._packet.token.toString())).to.be.undefined
+      setImmediate(done)
+    })
+    .end()
+    server.on('request', function(req, res) {
+      res.end(payload)
+    })
+  })
+
+  it('should receive full response payload', function(done) {
+    var payload = Buffer.alloc(16*0xff+1)
+    var req = coap.request({
+        port: port
+    })
+    .setOption('Block1', Buffer.of(0x0)) // early negotation with block size = 16, almost 10000/16 = 63 blocks
+    .on('response', function(res) {
+      expect(res.payload).to.eql(payload)
+      //expect(cache.get(res._packet.token.toString())).to.not.be.undefined
+      setImmediate(done)
+    })
+    .end()
+    server.on('request', function(req, res) {
+      res.end(payload)
+    })
+  })
+
+
+  function sendNextBlock1(req_token, req_block1_num) {
+    var packet = {
+        messageId: 1100 + req_block1_num
+      , token: req_token
+      , options: [{
+            name: 'Block1'
+          , value: Buffer.of(req_block1_num << 4)
+        }]
+    }
+    send(generate(packet))
+  }
+
+  function parallelBlock1Test(done, checkNReq, checkBlock1Message, checkNormalReq) {
+    var payload_len = 32+16+1
+    var payload_req1 = Buffer.alloc(payload_len)
+    var payload_req2 = Buffer.alloc(payload_len)
+    var req1_token = Buffer.alloc(4)
+    var req1_done = false
+    var req2_done = false
+    var req1_block1_num = 0
+    var req_client2 = coap.request({
+        port: port
+    })
+
+    fillPayloadBuffer(payload_req1)
+    fillPayloadBuffer(payload_req2)
+    fillPayloadBuffer(req1_token)
+
+    var nreq = 1;
+    server.on('request', function(req, res) {
+      // only two request to upper level, blockwise transfer completed from cache
+      if (nreq == 1)
+        res.end(payload_req1)
+      else if (nreq == 2)
+        res.end(payload_req2)
+
+      checkNReq(nreq)
+
+      nreq++
+    })
+
+    // Send first request, initiate blockwise transfer from server
+    sendNextBlock1(req1_token, req1_block1_num)
+
+    client.on('message', function(msg, rinfo) {
+      checkBlock1Message(msg, payload_req1, req1_block1_num, payload_len)
+
+      var expectMore = (req1_block1_num + 1) * 16 <= payload_len
+      if (expectMore) {
+        // Request next block after 50 msec delay
+        req1_block1_num++
+
+        setTimeout(function() {
+          // Send next request, fetch next block of blockwise transfer from server
+          sendNextBlock1(req1_token, req1_block1_num)
+        }, 50)
+      } else {
+        // No more blocks, transfer completed.
+        req1_done = true
+        if (req1_done && req2_done)
+          setImmediate(done)
+        }
+    })
+
+    req_client2.setOption('Block1', Buffer.of(0x10)) // request from block 1, with size = 16
+
+    // Delay second request so that first request gets first packet
+    setTimeout(function() {
+      req_client2.end()
+    }, 1)
+
+    req_client2.on('response', function(res) {
+      checkNormalReq(res, payload_req2)
+
+      req2_done = true
+      if (req1_done && req2_done)
+        setImmediate(done)
+    })
+  }
+
+  function checkNothing() {
+  }
+
+
+
 })
