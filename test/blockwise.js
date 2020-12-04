0487b4d8351770ca168f4d68e54a3192617c0f55
Braydon Harris <braydonharris@gmail.com>
2020-12-04 13:34:33 -0700

    Code clean up Add unit tests

--- test/blockwise.js
+++ test/blockwise.js
@@ -6,12 +6,14 @@
  * See the included LICENSE file for more details.
  */
 
+const { expect } = require('chai')
+
 var coap        = require('../')
   , parse       = require('coap-packet').parse
   , generate    = require('coap-packet').generate
   , getOption   = require('../lib/helpers').getOption
   , parseBlock2 = require('../lib/helpers').parseBlock2
-  , parseBlock1 = require('../lib/helpers').parseBlock1
+  , block = require('../lib/block')
   , dgram       = require('dgram')
 
 describe('blockwise2', function() {
@@ -356,173 +358,70 @@ describe('blockwise2', function() {
 })
 
 describe('blockwise1', () =>{
-  var server
-  , port
-  , clientPort
-  , client
-  , bufferVal
-  , payload   = Buffer.alloc(1536)
 
-  beforeEach(function(done) {
-    bufferVal = 0
-    port = nextPort()
-    server = coap.createServer()
-    server.listen(port, done)
-  })
+ describe('Generate Block Options', () => {
+   it('it should return buffer', (done) => {
+    var payload  = Buffer.of(0x01) 
+    let value = block.generateBlockOption(0, 0, 1);
+    expect(payload).to.eql(value)
+    setImmediate(done);
+   })
 
-  beforeEach(function(done) {
-    clientPort = nextPort()
-    client = dgram.createSocket('udp4')
-    client.bind(clientPort, done)
-  })
+   it('it should throw Invalid Parameters error', (done) => {
+    expect(() => {
+      block.generateBlockOption(0, null, undefined)
+    }).to.throw("Invalid parameters")
+    setImmediate(done);
+   })
 
-  afterEach(function() {
-    server.close()
-    client.close()
-  })
+   it('it should throw Sequence error', (done) => {
+    expect(() => {
+      block.generateBlockOption(1048576, 0, 0)
+    }).to.throw("Sequence number out of range")
+    setImmediate(done);
+   })
+ })
 
-  function send(message) {
-    client.send(message, 0, message.length, port, '127.0.0.1')
-  }
-
-  function nextBufferVal() {
-    if (bufferVal > 255)
-      bufferVal = 0
-    return bufferVal++
-  }
-
-  function fillPayloadBuffer(buffer) {
-    for (var i = 0; i < buffer.length; i++) {
-      buffer[i] = nextBufferVal()
+ describe('Parse Block Options', () => {
+  it('it should return object', (done) => {
+    var payload  = Buffer.of(0x01) 
+    var response = {
+      sequenceNumber: 0,
+      moreBlocks: 0,
+      blockSize: 1
     }
-    return buffer
-  }
+    let value = block.parseBlockOption(payload);
+    expect(value).to.eql(response)
+    setImmediate(done);
+   })
 
-  it('should server not use blockwise in response when payload fit in one packet', function(done) {
-    var payload   = Buffer.alloc(100)         // default max packet is 1280
+   it('it should throw Invalid Block Option error', (done) => {
+    var payload  = Buffer.from([0x04, 0x01, 0x03, 0x04])
+    expect(() => {
+      block.parseBlockOption(payload)
+    }).to.throw("Invalid block option buffer length. Must be 1, 2 or 3. It is 4")
+    setImmediate(done);
+   })
+ })
 
-    var req = coap.request({
-        port: port
-    })
-    .on('response', function(res) {
-      var blockwiseResponse = false
-      for (var i in res.options) {
-        if (res.options[i].name == 'Block1') {
-          blockwiseResponse = true
-          break
-        }
-      }
-      expect(blockwiseResponse).to.eql(false)
-      //expect(cache.get(res._packet.token.toString())).to.be.undefined
-      setImmediate(done)
-    })
-    .end()
-    server.on('request', function(req, res) {
-      res.end(payload)
-    })
-  })
+ describe('Exponenent to Byte Size', () => {
+  it('it should return value', (done) => {
+    let response = 1024;
+    let payload = 6;
+    let value = block.exponentToByteSize(payload);
+    expect(value).to.eql(response)
+    setImmediate(done);
+   })
+ })
 
-  it('should receive full response payload', function(done) {
-    var payload = Buffer.alloc(16*0xff+1)
-    var req = coap.request({
-        port: port
-    })
-    .setOption('Block1', Buffer.of(0x0)) // early negotation with block size = 16, almost 10000/16 = 63 blocks
-    .on('response', function(res) {
-      expect(res.payload).to.eql(payload)
-      //expect(cache.get(res._packet.token.toString())).to.not.be.undefined
-      setImmediate(done)
-    })
-    .end()
-    server.on('request', function(req, res) {
-      res.end(payload)
-    })
-  })
-
-
-  function sendNextBlock1(req_token, req_block1_num) {
-    var packet = {
-        messageId: 1100 + req_block1_num
-      , token: req_token
-      , options: [{
-            name: 'Block1'
-          , value: Buffer.of(req_block1_num << 4)
-        }]
-    }
-    send(generate(packet))
-  }
-
-  function parallelBlock1Test(done, checkNReq, checkBlock1Message, checkNormalReq) {
-    var payload_len = 32+16+1
-    var payload_req1 = Buffer.alloc(payload_len)
-    var payload_req2 = Buffer.alloc(payload_len)
-    var req1_token = Buffer.alloc(4)
-    var req1_done = false
-    var req2_done = false
-    var req1_block1_num = 0
-    var req_client2 = coap.request({
-        port: port
-    })
-
-    fillPayloadBuffer(payload_req1)
-    fillPayloadBuffer(payload_req2)
-    fillPayloadBuffer(req1_token)
-
-    var nreq = 1;
-    server.on('request', function(req, res) {
-      // only two request to upper level, blockwise transfer completed from cache
-      if (nreq == 1)
-        res.end(payload_req1)
-      else if (nreq == 2)
-        res.end(payload_req2)
-
-      checkNReq(nreq)
-
-      nreq++
-    })
-
-    // Send first request, initiate blockwise transfer from server
-    sendNextBlock1(req1_token, req1_block1_num)
-
-    client.on('message', function(msg, rinfo) {
-      checkBlock1Message(msg, payload_req1, req1_block1_num, payload_len)
-
-      var expectMore = (req1_block1_num + 1) * 16 <= payload_len
-      if (expectMore) {
-        // Request next block after 50 msec delay
-        req1_block1_num++
-
-        setTimeout(function() {
-          // Send next request, fetch next block of blockwise transfer from server
-          sendNextBlock1(req1_token, req1_block1_num)
-        }, 50)
-      } else {
-        // No more blocks, transfer completed.
-        req1_done = true
-        if (req1_done && req2_done)
-          setImmediate(done)
-        }
-    })
-
-    req_client2.setOption('Block1', Buffer.of(0x10)) // request from block 1, with size = 16
-
-    // Delay second request so that first request gets first packet
-    setTimeout(function() {
-      req_client2.end()
-    }, 1)
-
-    req_client2.on('response', function(res) {
-      checkNormalReq(res, payload_req2)
-
-      req2_done = true
-      if (req1_done && req2_done)
-        setImmediate(done)
-    })
-  }
-
-  function checkNothing() {
-  }
-
-
+ describe('Byte Size to Exponenet', () => {
+  it('it should return value', (done) => {
+    let response = 1024;
+    let payload = 6;
+    let value = block.byteSizeToExponent(response);
+    expect(value).to.eql(payload)
+    setImmediate(done);
+   })
+ })
 
 })
---
+++ test/helpers.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2013-2020 node-coap contributors.
+ *
+ * node-coap is licensed under an MIT +no-false-attribs license.
+ * All rights not explicitly granted in the MIT license are reserved.
+ * See the included LICENSE file for more details.
+ */
+
+const { expect } = require('chai');
+const { reporters } = require('mocha');
+const getOption = require('../lib/helpers').getOption;
+const hasOption = require('../lib/helpers').hasOption;
+const simplifyPacketForPrint = require('../lib/helpers').simplifyPacketForPrint;
+
+describe('Helpers', () => {
+
+    describe('Has Options', () => {
+        it('Should return true', (done) => {
+            let options = [
+                {name: 'test'},
+                {name: 'test2'}
+            ]
+            expect(hasOption(options, 'test')).to.eql(true)
+            setImmediate(done)
+        })
+
+        it('Should return null', (done) => {
+            let options = [
+                {name: 'test2'}
+            ]
+            expect(hasOption(options, 'test')).to.eql(null)
+            setImmediate(done)
+        })
+    })
+
+    describe('Get Options', () => {
+        it('Should return option value', (done) => {
+            let options = [
+                {name: 'test', value: 'hello'},
+                {name: 'test2', value: 'world'}
+            ]
+            expect(getOption(options, 'test')).to.eql('hello')
+            setImmediate(done)
+        })
+
+        it('Should return null', (done) => {
+            let options = [
+                {name: 'test2', value: 'world'}
+            ]
+            expect(getOption(options, 'test')).to.eql(null)
+            setImmediate(done)
+        })
+    })
+    
+    describe('Simplify Packet for Print', () => {
+        it('Should return pretty packet', (done) => {
+            let packet = {
+                token: '0x01',
+                options: [],
+                payload: '01 02 03'
+            }
+            let response = {
+                options: {},
+                payload: "Buff: 8",
+                token: "0x01"
+            }
+            expect(simplifyPacketForPrint(packet)).to.eql(response);
+            setImmediate(done)
+        })
+    })
+
+    // genAck
+
+    // addSetOptions
+
+    // toCode
+
+    // packetToMessage
+
+    // removeOption
+
+    // or
+
+    // isOptions
+
+    // isNumeric
+
+    // isBoolean
+})
