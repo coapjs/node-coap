55ed0fe17437d95ddbeaf1bd3c370c60e21a9ac1
Jan Romann <jan.romann@uni-bremen.de>
2021-09-28 02:44:05 +0200

    feat: add type declaration for ObserveWriteStream

---
+++ lib/observe_write_stream.d.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 node-coap contributors.
+ *
+ * node-coap is licensed under an MIT +no-false-attribs license.
+ * All rights not explicitly granted in the MIT license are reserved.
+ * See the included LICENSE file for more details.
+ */
+
+import { Writable } from 'stream'
+import { ParsedPacket } from 'coap-packet'
+import { setOption } from '../index'
+
+// TODO: Inherit from OutgoingMessage
+export default class ObserveWriteStream extends Writable {
+    constructor(request: ParsedPacket, send: (req: ObserveWriteStream, packet: ParsedPacket) => void)
+    _write(data: Buffer, encoding: BufferEncoding, done: (error?: Error | null) => void): void
+    _doSend (data: Buffer): void
+    reset (): void
+    setOption: typeof setOption
+    setHeader: typeof setOption
+}
