d67f693fa0770fcf73513d3301a62278c4073331
Jan Romann <jan.romann@uni-bremen.de>
2021-09-28 02:53:27 +0200

    docs: add missing copyright header (#281)

--- lib/outgoing_message.d.ts
+++ lib/outgoing_message.d.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021 node-coap contributors.
+ *
+ * node-coap is licensed under an MIT +no-false-attribs license.
+ * All rights not explicitly granted in the MIT license are reserved.
+ * See the included LICENSE file for more details.
+ */
+
 import { OptionName, ParsedPacket } from 'coap-packet'
 import { Writable } from 'stream'
 import { OptionValue, setOption } from '..'
