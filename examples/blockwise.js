f6bc487f78bbb7720490e71baf13bf18f4c81025
Jan Romann <jan.romann@uni-bremen.de>
2021-09-29 16:34:58 +0200

    fix(examples): fix problem caused by `new URL` (#287)

--- examples/blockwise.js
+++ examples/blockwise.js
@@ -1,9 +1,15 @@
 const coap = require('../') // or coap
 
 coap.createServer((req, res) => {
-    const path = new URL(req.url)
-    const time = parseInt(path.search.split('=')[1])
-    const pathname = path.pathname.split('/')[1]
+    // FIXME: This has became a bit ugly due to the
+    //        replacement of the depracated url.parse
+    //        with the URL constructor.
+    //        This part of the exmample should be replaced
+    //        once a nicer solution has been found.
+
+    const splitURL = req.url.split('?')
+    const time = parseInt(splitURL[1].split('=')[1])
+    const pathname = splitURL[0].split('/')[1]
 
     res.end(new Array(time + 1).join(pathname + ' '))
 }).listen(() => {
